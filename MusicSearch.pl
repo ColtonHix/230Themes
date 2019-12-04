4a12f93796b1b21cada6af49403e51423e01b61d
norinats <47509793+norinats@users.noreply.github.com>
2019-12-04 12:09:01 -0700

    Update MusicSearch.pl

--- MusicSearch.pl
+++ MusicSearch.pl
@@ -8,13 +8,40 @@ print "Please type a file name: ";
 my $filename = <STDIN>;
 chomp $filename;
 
+open(FH, '<', $filename) or die $!;
+
+sub searchType {
+    print "\nWhat do you want to search by? :";
+    my $searchParameter = <STDIN>;
+
+    if ($searchParameter =~ m/title/) {
+        print "What song do you want to search for? : ";
+        my $title = <STDIN>;
+    
+    } elseif ($searchParameter =~ m/time/) {
+        print "How long do you want the songs to be? : ";
+        my $timeframe = <STDIN>;
+    
+    } elseif ($searchParameter =~ m/album/) {
+        print "What album are you searching for? : ";
+        my $album = <STDIN>;
+    
+    } elseif ($searchParameter =~ m/artist/) {
+        print "What artist are you looking for? : ";
+        my $artist = <STDIN>;
+    
+    } else {
+        print "Invalid parameter\n";
+        print "ex. title, time, album, artist\n";
+        searchType();
+    }
+}
+
 #get what section you are search through
     #ex. title/song title/song, length/time/song length, album, artist
 #get boundaries of what you are searching for
     # all titles/artists/albums that start with a/contain 'the'
     # 
-  
-open(FH, '<', $filename) or die $!;
    
 while(<FH>){
     print $_;
