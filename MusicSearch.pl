2692a5a1068ded1aaa85a8c58eac73d11f84eb3b
ColtonHix <coltonhix@u.boisestate.edu>
2019-12-04 14:41:42 -0700

    Added csv module
    Also fixed invalid lines in our csv

--- MusicSearch.pl
+++ MusicSearch.pl
@@ -1,46 +1,52 @@
 #!/usr/bin/perl
 use warnings;
 use strict;
- 
+use lib "Text";
+use Text::CSV;
+
 print "Please type a file name: ";
 #have a set file name not stdin
-#use a csv file type: song title | song length | artist | album 
+#use a csv file type: song title | song length | artist | album
 my $filename = <STDIN>;
 chomp $filename;
 
-open(FH, '<', $filename) or die $!;
+#Initializes csv module.
+my $csv = Text::CSV->new({ sep_char => ',' });
+
+open(my $fh, '<', $filename) or die $!;
 
 #get what section you are search through
     #ex. title/song title/song, length/time/song length, album, artist
 sub searchType {
     print "\nWhat do you want to search by? :";
     my $searchParameter = <STDIN>;
+    #chomp removes trailing newlines
+    chomp($searchParameter);
+        if ($searchParameter eq 'title') {
+            print "What song do you want to search for? : ";
+            my $title = <STDIN>;
+            titleSearch($title);
 
-    if ($searchParameter =~ m/title/) {
-        print "What song do you want to search for? : ";
-        my $title = <STDIN>;
-        titleSearch($title);
-    
-    } elsif ($searchParameter =~ m/time/) {
-        print "How long do you want the songs to be? : ";
-        my $timeframe = <STDIN>;
-        timeSearch($timeframe);
-    
-    } elsif ($searchParameter =~ m/album/) {
-        print "What album are you searching for? : ";
-        my $album = <STDIN>;
-        albumSearch($album);
-    
-    } elsif ($searchParameter =~ m/artist/) {
-        print "What artist are you looking for? : ";
-        my $artist = <STDIN>;
-        artistSearch($artist);
-    
-    } elsif ($searchParameter =~ m/genre/) {
-        print "What genre are you looking for? : ";
-        my $genre = <STDIN>;
-        genreSearch($genre);
-        
+        } elsif ($searchParameter eq 'time') {
+            print "How long do you want the songs to be? : ";
+            my $timeframe = <STDIN>;
+            timeSearch($timeframe);
+
+        } elsif ($searchParameter eq 'album') {
+            print "What album are you searching for? : ";
+            my $album = <STDIN>;
+            albumSearch($album);
+
+        } elsif ($searchParameter eq 'artist') {
+            print "What artist are you looking for? : ";
+            my $artist = <STDIN>;
+            artistSearch($artist);
+
+        } elsif ($searchParameter eq 'genre') {
+            print "What genre are you looking for? : ";
+            my $genre = <STDIN>;
+            genreSearch($genre);
+
     }else {
         print "Invalid parameter\n";
         print "ex. title, time, album, artist, genre\n";
@@ -51,27 +57,37 @@ sub searchType {
 #get boundaries of what you are searching for
     # all titles/artists/albums that start with a/contain 'the'
 sub titleSearch {
-    
+
 }
 
 sub timeSearch {
-    
+
 }
 
 sub albumSearch {
-    
+
 }
 
 sub artistSearch {
-    
+
 }
 
 sub genreSearch {
-    
+
 }
-   
-while(<FH>){
-    print $_;
+
+#skips the header row
+my $dummy = <$fh>;
+while(my $row = <$fh>){
+  if ($csv->parse($row)) {
+    my @vals = $csv->fields();
+
+    foreach(@vals) {
+      print "$_\n";
     }
-       
-close(FH);
+} else {
+    warn "Line could not be parsed: $row\n $.";
+}
+    }
+
+close($fh);
