787bcef918ba41ac7d3a14dd66d8a0d4855d5230
norinats <47509793+norinats@users.noreply.github.com>
2019-12-04 12:26:12 -0700

    Update MusicSearch.pl

--- MusicSearch.pl
+++ MusicSearch.pl
@@ -10,6 +10,8 @@ chomp $filename;
 
 open(FH, '<', $filename) or die $!;
 
+#get what section you are search through
+    #ex. title/song title/song, length/time/song length, album, artist
 sub searchType {
     print "\nWhat do you want to search by? :";
     my $searchParameter = <STDIN>;
@@ -17,31 +19,56 @@ sub searchType {
     if ($searchParameter =~ m/title/) {
         print "What song do you want to search for? : ";
         my $title = <STDIN>;
+        titleSearch($title);
     
     } elseif ($searchParameter =~ m/time/) {
         print "How long do you want the songs to be? : ";
         my $timeframe = <STDIN>;
+        timeSearch($timeframe);
     
     } elseif ($searchParameter =~ m/album/) {
         print "What album are you searching for? : ";
         my $album = <STDIN>;
+        albumSearch($album);
     
     } elseif ($searchParameter =~ m/artist/) {
         print "What artist are you looking for? : ";
         my $artist = <STDIN>;
+        artistSearch($artist);
     
-    } else {
+    } elseif ($searchParameter =~ m/genre/) {
+        print "What genre are you looking for? : ";
+        my $genre = <STDIN>;
+        genreSearch($genre);
+        
+    }else {
         print "Invalid parameter\n";
-        print "ex. title, time, album, artist\n";
+        print "ex. title, time, album, artist, genre\n";
         searchType();
     }
 }
 
-#get what section you are search through
-    #ex. title/song title/song, length/time/song length, album, artist
 #get boundaries of what you are searching for
     # all titles/artists/albums that start with a/contain 'the'
-    # 
+sub titleSearch {
+    
+}
+
+sub timeSearch {
+    
+}
+
+sub albumSearch {
+    
+}
+
+sub artistSearch {
+    
+}
+
+sub genreSearch {
+    
+}
    
 while(<FH>){
     print $_;
